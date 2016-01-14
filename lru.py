d73753a590eda3f9b70d09d9ca576cc389a6a383
hugos94 <hugos94@gmail.com>
2016-01-14 14:17:02 -0300

    LRU corrigido e teste2 adicionado.

--- lru.py
+++ lru.py
@@ -12,25 +12,23 @@ class LRU(object):
         requisitions = copy.deepcopy(inputs) # Realiza a copia profunda das entradas para uma variavel local
 
         frames = [] # Lista que armazena as paginas
-        used_frames = []
-
+        list_used = [] # Lista de paginas ja utilizadas
         miss = 0 # Contador de miss
 
-        for page in requisitions: # Iteracao na lista de requisicoes
-            candidate = -1 # Inicializa o candidato a ser retirado como -1
-            value = float("inf") # Inicializa o valor de comparacao como infinito
-            if not page in frames: # Verifica se a lista possui a pagina
+        for position,page in enumerate(requisitions): # Iteracao na lista de requisicoes
+            if page not in frames: # Verifica se a lista possui a pagina
+                candidate_position = -1 # Posicao do possivel candidato a sair do frame
+                remove_candidate = -1 # Pagina candidata a sair da lista
                 if (len(frames) == frame_size): # Verifica se a lista esta cheia
-                    for p in range(frame_size): # Iteracao nas paginas alocadas
-                        if (used_frames[p] < value): # Se o contador de hit de cada pagina for menor que o valor minimo
-                            value = used_frames[p] # Valor minimo recebe o valor de hit da pagina
-                            candidate = p # candidate recebe o candidato a ser retirado
-                    frames.pop(candidate) # candidate é removido da lista
-                    used_frames.pop(candidate) # o contador de hit do candidate é removido da lista
+                    reverse_list = list_used[:] # Copia a lista de paginas ja utilizadas
+                    reverse_list.reverse() # Reverte a lista para encontrar a pagina referenciada mais antiga no frame
+                    for position,candidate in enumerate(frames): # Iteracao nas paginas existentes no frame
+                        if(candidate_position <= reverse_list.index(candidate)): # Verifica se a pagina do frame foi utilizada com o maior tempo
+                            candidate_position = reverse_list.index(candidate) # Troca o candidato a sair da lista
+                            remove_candidate = position # Troca a posicao do candidato a sair da lista
+                    frames.pop(remove_candidate) # Remove o candidato escolhido do frame
                 miss+=1 # Contador de miss incrementado
                 frames.append(page) # Pagina referenciada é adicionada a lista
-                used_frames.append(int(1)) # Contador de hit da pagina referenciada é adicionada na lista
-            else:
-                used_frames[frames.index(page)] += 1 # Contador de hit da pagina referenciada e incrementado
+            list_used.append(page) # Adiciona a pagina atual a lista de paginas utilizadas
 
         print("LRU " + str(miss)) # Imprime a saida do LRU
